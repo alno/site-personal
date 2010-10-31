976f9d82cc8db2c55fc075ae78de4087106a89bc
Alexey Noskov <alexey.noskov@gmail.com>
2010-10-31 23:07:39 +0300

    Nested layouts, page description

--- application.rb
+++ application.rb
@@ -1,10 +1,16 @@
 require '../rayo/lib/rayo.rb'
 
+require 'redcloth'
+
 class Application < Rayo::Application
 
   configure do |c|
     c.content_dir = File.join( File.dirname(__FILE__), 'content' )
     c.languages = ['ru','en']
+
+    c.add_filter 'textile' do |source|
+      RedCloth.new( source ).to_html
+    end
   end
 
   set :public, File.join( File.dirname(__FILE__), 'public' )
