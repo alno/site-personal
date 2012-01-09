21b86fa29eb7d9679fecb95c14a96ee00e497e3b
Alexey Noskov <alexey.noskov@gmail.com>
2012-01-09 14:22:43 +0400

    Update redcloth

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,46 +1,38 @@
 set :application, "personal"
 set :repository,  "git://github.com/alno/site-personal.git"
 
-set :user, "hosting_alno"
+set :user, "personal"
 set :use_sudo, false
-set :deploy_to, "/home/hosting_alno/projects/personal"
+set :deploy_to, "/home/personal/apps/personal"
 
 set :scm, :git
 
-role :web, "hydrogen.locum.ru"
-role :app, "hydrogen.locum.ru"
-role :db,  "hydrogen.locum.ru", :primary => true
+role :web, "alno.name"
+role :app, "alno.name"
+role :db,  "alno.name", :primary => true
 
-set :bundle, "/var/lib/gems/1.8/bin/bundle"
-set :unicorn, "/var/lib/gems/1.8/bin/unicorn"
-set :unicorn_conf, "/etc/unicorn/personal.alno.rb"
-set :unicorn_pid, "/var/run/unicorn/personal.alno.pid"
+require 'bundler/capistrano'
 
-after "deploy:update_code", :bundle_deps
-after "deploy:update_code", :copy_configs
-
-task :bundle_deps, roles => :app do
-  run "cd #{release_path} && #{bundle} install --path ~/.gem --without development test"
-end
-
-task :copy_configs, roles => :app do
-  run "cp #{deploy_to}/shared/flickr.yml #{release_path}/config"
-  run "cp #{deploy_to}/shared/slideshare.yml #{release_path}/config"
+after "deploy:update_code", roles => :app do
+  run "ln -s #{shared_path}/config/unicorn.rb #{release_path}/config/unicorn.rb"
+  run "ln -s #{shared_path}/config/flickr.yml #{release_path}/config/flickr.yml"
+  run "ln -s #{shared_path}/config/slideshare.yml #{release_path}/config/slideshare.yml"
 end
 
 namespace :deploy do
-  desc "Start application"
+
+  desc "Restarting unicorn"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "cd #{deploy_to}/current ; ([ -f tmp/pids/unicorn.pid ] && kill -USR2 `cat tmp/pids/unicorn.pid`) || bundle exec unicorn -c config/unicorn.rb -E production -D"
+  end
+
+  desc "Rude restart application"
+  task :rude_restart, :roles => :app do
+    run "cd #{deploy_to}/current ; pkill unicorn; sleep 0.5; pkill -9 unicorn; sleep 0.5 ; bundle exec unicorn_rails -c config/unicorn.rb -E production -D "
+  end
+
   task :start, :roles => :app do
-    run "cd #{deploy_to}/current && #{bundle} exec #{unicorn} -Dc #{unicorn_conf}"
+    rude_restart
   end
 
-  desc "Stop application"
-  task :stop, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
-  end
-
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || (cd #{deploy_to}/current && #{bundle} exec #{unicorn} -Dc #{unicorn_conf})"
-  end
 end
